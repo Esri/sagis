eb454a3dd5982742d493b012cfbd7f7d49bf4d02
Stefan Eilemann <SEilemann@esri.com>
2023-11-14 16:37:10 +0100

    Synchronize more state, ported from the prototype

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,10 +1,23 @@
+import Basemap from "@arcgis/core/Basemap";
+import CameraLayout from "@arcgis/core/CameraLayout";
+import Ground from "@arcgis/core/Ground";
 import Viewpoint from "@arcgis/core/Viewpoint";
+import WebScene from "@arcgis/core/WebScene";
+import Collection from "@arcgis/core/core/Collection";
+import Layer from "@arcgis/core/layers/Layer";
 import SceneView from "@arcgis/core/views/SceneView";
 import { watch, whenOnce } from "@arcgis/core/core/reactiveUtils";
 import Handles from "@arcgis/core/core/Handles";
 import { Layout } from "./Layout";
 
-/** Interface to or from a distributed viewing session */
+/** Interface to or from a distributed viewing session.
+ *
+ * The controller on the viewer sends messages to the server, which re-broadcasts this message to all clients. Only
+ * a special tile message is interpreted by the server, which keeps the global display wall layout state.
+ *
+ * Messages are one character for the message type, followed by the message payload. The payload is typically the state
+ * in JSON. The viewer and client side deserialization and serialization must match for correct operation.
+ */
 export class Controller {
   constructor(
     private readonly _view: SceneView,
@@ -27,33 +40,82 @@ export class Controller {
 
   private _initViewer(): void {
     const initial = { initial: true };
-    const broadcastValue = (type: string, value: any): void => {
+    const broadcastValue = (type: string, value: any, context?: any): void => {
       if (value != null && this._socket.readyState === WebSocket.OPEN) {
         if (typeof value === "object" && "toJSON" in value) {
-          value = value.toJSON();
+          value = value.toJSON(context);
         }
         const message = typeof value === "string" ? value : JSON.stringify(value);
         this._socket.send(`${type}${message}`);
       }
     };
 
+    const watchLayers = (layers: Array<Layer> | Collection<Layer>): void =>
+      layers.forEach((layer) =>
+        this._handles.add(
+          watch(
+            () => layer.visible,
+            (visible) => broadcastValue("l", `${layer.id} ${visible}`)
+          ),
+          layer.id
+        )
+      );
+
     this._handles.add([
+      watch(
+        () => this._view.environment.lighting,
+        () => broadcastValue("L", this._view.environment.lighting),
+        initial
+      ),
+      watch(
+        () => this._view.environment.weather,
+        () => broadcastValue("C", this._view.environment.weather),
+        initial
+      ),
       watch(
         () => this._view.viewpoint,
         (viewpoint) => broadcastValue("V", viewpoint),
         initial
-      )
+      ),
+      watch(
+        () => this._view.qualityProfile,
+        (mode) => broadcastValue("Q", mode),
+        initial
+      ),
+      watch(
+        () => this._view.map,
+        (map) => broadcastValue("W", map),
+        initial
+      ),
+      watch(
+        () => this._view.map.basemap,
+        (basemap) => broadcastValue("B", basemap, { origin: "web-scene" }), // origin needed for SceneLayer serialization
+        initial
+      ),
+      watch(
+        () => this._view.map.ground,
+        (ground) => broadcastValue("G", ground),
+        initial
+      ),
+      this._view.map.allLayers.on("change", (event) => {
+        this._handles.remove(event.removed.map((layer) => layer.id));
+        watchLayers(event.added);
+      })
     ]);
+
+    watchLayers(this._view.map.allLayers);
   }
 
   private _initClient(layout: Layout): void {
-    console.log(`T${layout.row},${layout.column}`);
+    const cameraLayout = new CameraLayout();
+    cameraLayout.row = layout.row;
+    cameraLayout.column = layout.column;
+
     this._socket.onopen = () =>
       // Registering as a client will push global state, wait for not updating st all initial state is set
       whenOnce(() => !this._view.updating).then(() => {
         const camera = this._view.camera;
-        camera.layout.row = layout.row;
-        camera.layout.column = layout.column;
+        camera.layout = cameraLayout.clone();
         this._view.camera = camera;
 
         this._socket.send(`T${layout.row},${layout.column}`);
@@ -69,19 +131,65 @@ export class Controller {
       } catch (e) {}
 
       switch (type) {
-        case "V":
-          this._view.viewpoint = Viewpoint.fromJSON(json);
+        case "V": {
+          const viewpoint = Viewpoint.fromJSON(json);
+          viewpoint.camera.layout = cameraLayout.clone();
+          this._view.viewpoint = viewpoint;
+          break;
+        }
+
+        case "L":
+          this._view.environment.lighting = json;
+          break;
+
+        case "Q":
+          this._view.qualityProfile = message as "low" | "medium" | "high";
+          break;
+
+        case "W": {
+          WebScene.fromJSON(json)
+            .load()
+            .then((map: WebScene) => (this._view.map = map));
+          break;
+        }
+
+        case "C":
+          this._view.environment.weather = json;
+          break;
+
+        case "B": {
+          Basemap.fromJSON(json, { origin: "web-scene" }) // origin needed for SceneLayer serialization
+            .load()
+            .then((basemap: Basemap) => (this._view.map.basemap = basemap));
+          break;
+        }
+
+        case "G":
+          this._view.map.ground = Ground.fromJSON(json);
           break;
 
         case "T": {
-          const camera = this._view.camera;
           const tile = message.split(",");
+          cameraLayout.rows = parseFloat(tile[0]);
+          cameraLayout.columns = parseFloat(tile[1]);
 
-          camera.layout.rows = parseFloat(tile[0]);
-          camera.layout.columns = parseFloat(tile[1]);
+          const camera = this._view.camera;
+          camera.layout = cameraLayout.clone();
           this._view.camera = camera;
           break;
         }
+
+        case "l": {
+          const [id, visible] = message.split(" ");
+          const layer = this._view.map.allLayers.find((layer) => layer.id === id);
+          if (layer) {
+            layer.visible = visible === "true";
+          }
+          break;
+        }
+
+        default:
+          console.error(`Missing deserialization for type '${type}'`);
       }
     };
   }
--- src/client.ts
+++ src/client.ts
@@ -5,14 +5,17 @@ import { Controller } from "./Controller";
 
 const params = new Map<string, string>();
 
-const matches = window.parent.location.href.match(/[?&]+([^=&]+)=([^&]*)/gi);
-matches?.forEach((_match, _index, matches) => params.set(matches[0], matches[1]));
+const matches = window.parent.location.href.match(/[?&]+([^&]+)/gi);
+matches?.forEach((match, _index) => {
+  const tokens = match.split("=");
+  params.set(tokens[0].substring(1), tokens[1]);
+});
 
 const view = new SceneView({ container: "SceneView", map: { basemap: "topo-3d", ground: "world-elevation" } });
 const layout = new Layout(params.get("row"), params.get("column"));
 const server = params.get("server") ?? "localhost";
 
-// The view must be ready (or resolved) before you can access the properties of the WebScene
+// The view must be ready (or resolved) before you can access its properties
 view.when(() => {
   new Controller(view, server, layout);
 });
